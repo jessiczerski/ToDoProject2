04236ebc145490c0e7ac4032648166bb8ca0865a
jessiczerski <jjaneczerski8494@gmail.com>
2022-06-14 13:25:48 -0400

    Initial Commit

---
+++ ToDoProject2/CompleteToDoViewController.swift
@@ -0,0 +1,47 @@
+//
+//  CompleteToDoViewController.swift
+//  ToDoProject2
+//
+//  Created by JESSI on 6/14/22.
+//
+
+import UIKit
+
+class CompleteToDoViewController: UIViewController {
+
+    var previousVC = ToDoTableViewController()
+    var selectedToDo : ToDoCD?
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        titleLabel.text = selectedToDo?.name
+    }
+    
+    @IBAction func completeTapped(_ sender: Any) {
+      if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+        if let theToDo = selectedToDo {
+          context.delete(theToDo)
+          navigationController?.popViewController(animated: true)
+        }
+      }
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ToDoProject2/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDoProject2
+//
+//  Created by JESSI on 6/14/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
